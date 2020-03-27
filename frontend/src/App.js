b0979bc1d6c45ab342cc8c2bc1b58f79e5581865
Mạnh Đinh <dinhthaimanh@gmail.com>
2020-03-27 11:03:47 +0700

    commit 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React app 1
+          Learn React app 2
         </a>
       </header>
     </div>
